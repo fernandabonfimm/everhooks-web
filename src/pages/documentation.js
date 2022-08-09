1ae4f76009e7253bc3c996e96518944f6e3953d1
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2022-08-09 10:53:53 -0300

    ensinando antd

--- src/pages/documentation.js
+++ src/pages/documentation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavbarComponent from "../components/Navbar";
 import FooterComponent from "../components/Footer";
-import { Layout } from "antd";
+import { Layout, Row, Col, Card } from "antd";
 import '../styles/pages/documentation.css';
 
 const {Content} = Layout;
@@ -12,7 +12,14 @@ const Documentation = () => {
         <Layout style={{ minHeight: "100vh" }}>
             <NavbarComponent/>
             <Content className="content-home">
-
+                <Row gutter={[32, 22]}>
+                    <Col xs={24} xl={12}>
+                        <Card><h3>Hello Wordl!</h3></Card>
+                    </Col>
+                    <Col xs={24} xl={12}>
+                        <Card><h3>Hello Wordl!</h3></Card>
+                    </Col>
+                </Row>
             </Content>
             <FooterComponent/>
         </Layout>
