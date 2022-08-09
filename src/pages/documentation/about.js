6847a05c121f5d645838d0684aeaf39938e0d266
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2022-08-09 15:37:28 -0300

    merge das branchs

--- src/pages/documentation/about.js
+++ src/pages/documentation/about.js
@@ -1,9 +1,9 @@
 import React from "react";
-import NavbarComponent from "../components/Navbar";
-import FooterComponent from "../components/Footer";
+import NavbarComponent from "../../components/Navbar";
+import FooterComponent from "../../components/Footer";
 import { Layout, Row, Col, Card } from "antd";
-import '../styles/pages/documentation.css';
-import SiderComponent from "../../../components/Sider";
+import '../../styles/pages/documentation.css';
+import SiderComponent from "../../components/Sider";
 
 const {Content} = Layout;
 
