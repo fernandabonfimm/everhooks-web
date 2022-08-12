1d984084829a06e7bb071c158bb896947a549a1b
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2022-08-12 15:16:24 -0300

    merge entre main e new-branch

--- src/pages/documentation/pages/about.js
+++ src/pages/documentation/pages/about.js
@@ -44,7 +44,7 @@ const About = () => {
                         <h2>Sobre o EverHook</h2>
                         <img src={aboutPng} width={200} />
                         <p>
-                          Lorem Ipsum is simply dummy text of the printing and
+                          Lorem Ipsum is simply, dummy text of the printing and
                           typesetting industry. Lorem Ipsum has been the
                           industry's standard dummy text ever since the 1500s,
                           when an unknown printer took a galley of type and
