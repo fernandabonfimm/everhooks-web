ab0a7fce604a005a789521f43a147fd7705204d0
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2022-08-22 13:34:50 -0300

    comsupiton

--- src/pages/details.js
+++ src/pages/details.js
@@ -7,6 +7,10 @@ import { MdOutlineContentCopy } from "react-icons/md";
 import { IoIosArrowUp } from "react-icons/io";
 import "../styles/pages/details.css";
 import TableHeaders from "../components/tables/TableHeaders";
+import {
+  getFirstId,
+  getUuid,
+} from "../../../../Documents/everhooks-web/src/services/routes/apiUuid";
 
 const { Content } = Layout;
 
@@ -15,6 +19,8 @@ const Details = () => {
   const [rawContent] = useState(
     "hottok=62743c4a5d8b21056fe2adfc&prod=62c84157604923283f4566ce&prod_name=OPERA%C3%87%C3%83O%20MILION%C381RIA&off=62d879bde8b23b66bf5f0f18&price=147.00&aff=&aff_name=&email=teste226g%40evermart.com.br&name=Teste%20Evermart&first_name=Teste&last_name=Evermart&doc=76194026081&phone_local_code=99&phone_number=999999999&phone_checkout_local_code=99&phone_checkout_number=999999999&address=&address_number=&address_country=Brasil&address_district=&address_comp=&address_city=&address_state=&address_zip_code=&transaction=PAY-59995KJ4CY43&xcod=&src=&utm_source=&utm_medium=&utm_campaign=&utm_term=&utm_content=&status=cancelled&payment_engine=evermart&payment_type=credit_card&hotkey=&name_subscription_plan=147%20OPM%2020.07.2022&subscriber_code=&recurrency_period=&cms_marketplace=7.34&cms_vendor=125.70&cms_aff=0.00&coupon_code=&callback_type=1&subscription_status=&transaction_ext=PAY-59995KJ4CY43&sck=&purchase_date=2022-07-22T18%3A14%3A35.542Z&confirmation_purchase_date=2022-07-22T18%3A14%3A35.542"
   );
+  const [uuidGet, setUuidGet] = useState([]);
+  const [uuidList, setUuidList] = useState([]);
   function copiarTexto() {
     let textoCopiado = document.getElementById("text");
     textoCopiado.select();
@@ -27,6 +33,60 @@ const Details = () => {
     textoCopiado.setSelectionRange(0, 99999);
     document.execCommand("copy");
   }
+
+  useEffect(() => {
+    loadData(uuidGet);
+  }, [uuidGet]);
+
+  const loadID = async () => {
+    try {
+      const _result = await getUuid();
+      console.log(_result);
+      if (_result?.response) {
+        setUuidGet(
+          _result?.data?.sort((a, b) => {
+            if (a && b) {
+              if (moment(a.createdAt).unix() < moment(b.createdAt).unix()) {
+                return 1;
+              }
+              if (moment(a.createdAt).unix() > moment(b.createdAt).unix()) {
+                return -1;
+              }
+            }
+            return 0;
+          })
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadData = async (uuid) => {
+    try {
+      const _result = await getFirstId(uuid);
+      console.log(_result);
+      if (_result?.response) {
+        setUuidList(
+          _result?.data?.sort((a, b) => {
+            if (a && b) {
+              if (moment(a.createdAt).unix() < moment(b.createdAt).unix()) {
+                return 1;
+              }
+              if (moment(a.createdAt).unix() > moment(b.createdAt).unix()) {
+                return -1;
+              }
+            }
+            return 0;
+          })
+        );
+      } else {
+        setUuidList([]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <Layout style={{ minHeight: "100vh" }}>
