b4ef62f0c74da47780fc113d865dc34b53d7ad92
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2022-08-25 13:12:12 -0300

    alteração de https publica da api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://everhook.herokuapp.com",
+  baseURL: "https://neweverhook.herokuapp.com",
 });
 
 export default api;
